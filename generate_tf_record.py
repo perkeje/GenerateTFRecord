2f8ed5230e9398f7cf5610c2a3c0d7e1fb182502
Juraj Perić <73022083+perkeje@users.noreply.github.com>
2023-06-06 22:54:05 +0200

    Add files via upload

--- generate_tf_record.py
+++ generate_tf_record.py
@@ -19,6 +19,7 @@ optional arguments:
 import os
 import glob
 import pandas as pd
+import hashlib
 import io
 import xml.etree.ElementTree as ET
 import argparse
@@ -84,15 +85,18 @@ def xml_to_csv(path):
             value = (root.find('filename').text,
                      int(root.find('size')[0].text),
                      int(root.find('size')[1].text),
-                     member[0].text,
-                     int(member[5][0].text),
-                     int(member[5][1].text),
-                     int(member[5][2].text),
-                     int(member[5][3].text)
+                     member['name'].text,
+                     member['pose'].text,
+                     member['truncated'].text,
+                     member['difficult'].text,
+                     int(member['bndbox'][0].text),
+                     int(member['bndbox'][1].text),
+                     int(member['bndbox'][2].text),
+                     int(member['bndbox'][3].text)
                      )
             xml_list.append(value)
     column_name = ['filename', 'width', 'height',
-                   'class', 'xmin', 'ymin', 'xmax', 'ymax']
+                   'class','pose', 'truncated','difficult', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     return xml_df
 
@@ -108,20 +112,24 @@ def split(df, group):
 
 
 def create_tf_example(group, path):
-    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
-        encoded_jpg = fid.read()
-    encoded_jpg_io = io.BytesIO(encoded_jpg)
-    image = Image.open(encoded_jpg_io)
+    with tf.io.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+        encoded_png = fid.read()
+    encoded_png_io = io.BytesIO(encoded_png)
+    image = Image.open(encoded_png_io)
     width, height = image.size
+    key = hashlib.sha256(encoded_png).hexdigest()
 
     filename = group.filename.encode('utf8')
-    image_format = b'jpg'
+    image_format = b'png'
     xmins = []
     xmaxs = []
     ymins = []
     ymaxs = []
     classes_text = []
     classes = []
+    truncated = []
+    poses = []
+    difficult_obj = []
 
     for index, row in group.object.iterrows():
         xmins.append(row['xmin'] / width)
@@ -130,20 +138,27 @@ def create_tf_example(group, path):
         ymaxs.append(row['ymax'] / height)
         classes_text.append(row['class'].encode('utf8'))
         classes.append(class_text_to_int(row['class']))
+        truncated.append(int(row['truncated']))
+        poses.append(row['pose'].encode('utf8'))
+        difficult_obj.append(int(row['difficult']))
 
     tf_example = tf.train.Example(features=tf.train.Features(feature={
         'image/height': dataset_util.int64_feature(height),
         'image/width': dataset_util.int64_feature(width),
         'image/filename': dataset_util.bytes_feature(filename),
         'image/source_id': dataset_util.bytes_feature(filename),
-        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+        'image/encoded': dataset_util.bytes_feature(encoded_png),
         'image/format': dataset_util.bytes_feature(image_format),
+        'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
         'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
         'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
         'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
         'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
         'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
         'image/object/class/label': dataset_util.int64_list_feature(classes),
+        'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
+        'image/object/truncated': dataset_util.int64_list_feature(truncated),
+        'image/object/view': dataset_util.bytes_list_feature(poses),
     }))
     return tf_example
 
